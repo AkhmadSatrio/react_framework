28c886384bfeae977740513b5e412ee164cef1a1
Akhmad <akhmadsatrio037@gmail.com>
2025-10-29 10:38:37 +0700

    Update 2

---
+++ src/pages/Kategory.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Kategory() {
+    return(
+        <div>
+            <h1>Selamat datang di Kategori</h1>
+        </div>
+    );
+}
+
+export default Kategory;
---
+++ src/pages/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Produk() {
+    return(
+        <div>
+            <h1>Selamat datang di Page Produk</h1>
+        </div>
+    );
+}
+
+export default Produk;

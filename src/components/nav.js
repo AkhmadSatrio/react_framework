7cd08f43ecbb075364ee6fe8c7145409b69dbe96
Akhmad <akhmadsatrio037@gmail.com>
2025-11-05 10:09:12 +0700

    Bootstrap In React

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/Product">Produk</Link></li>
-                <li><Link to="/Kategory">Kategori</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </nav>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link" href="/Product">Produk</a>
+                <a class="nav-link" href="/Kategory">Kategori</a>
+                <a class="nav-link" href="/contact">Contact</a>
+            </div>
+            </div>
+        </div>
+     </nav>
     );
-};
-
+}
 export default Navbar;
